d622763d7c5cdca149219a780748aa6d3fd3571c
Nicotrz <nicolas.trz@gmail.com>
2019-05-03 22:26:02 +0200

    Adding gitignore file

---
+++ BaluchonTests/FakeChangeData.swift
@@ -0,0 +1,9 @@
+//
+//  FakeChangeData.swift
+//  BaluchonTests
+//
+//  Created by Nicolas Sommereijns on 03/05/2019.
+//  Copyright © 2019 Nicolas Sommereijns. All rights reserved.
+//
+
+import Foundation
---
+++ BaluchonTests/URLSessionFake.swift
@@ -0,0 +1,9 @@
+//
+//  UrlSessionFake.swift
+//  BaluchonTests
+//
+//  Created by Nicolas Sommereijns on 03/05/2019.
+//  Copyright © 2019 Nicolas Sommereijns. All rights reserved.
+//
+
+import Foundation
